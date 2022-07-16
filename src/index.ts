6fc1f7d42c85e0a19af2190a533e5669cac5228a
dperujo <87599892+dperujo@users.noreply.github.com>
2022-07-16 22:04:21 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
 
+import db from "./queries";
+
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
 const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
-const db = require('./queries');
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
